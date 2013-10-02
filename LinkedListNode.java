1b49ee72e50ad9b4731b3639d5381473ad470d3f
ss237597 <ss237597@email.vccs.edu>
2013-10-02 11:10:08 -0400

    Create LinkedListNode.java

---
+++ LinkedListNode.java
@@ -0,0 +1,26 @@
+public class LinkedListNode <T>{
+	T element;
+	LinkedListNode <T> link;
+	public LinkedListNode(T element) {
+		super();
+		this.element = element;
+		this.link = null;
+	}
+	public LinkedListNode(T element, LinkedListNode <T> link) {
+		super();
+		this.element = element;
+		this.link = link;
+	}
+	public T getElement() {
+		return element;
+	}
+	public void setElement(T element) {
+		this.element = element;
+	}
+	public LinkedListNode <T> getLink() {
+		return link;
+	}
+	public void setLink(LinkedListNode <T>link) {
+		this.link = link;
+	}
+}
